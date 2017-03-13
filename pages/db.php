147f3a1c9eb6cd431348fbf51100a5f52ec93ed9
haoxiangzhx <haoxiangzhx@gmail.com>
2017-03-12 22:15:40 -0700

    delete some file

--- pages/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    $db = new mysqli('localhost', 'cs143', '', 'CS143');
-
-    if($db->connect_errno > 0) {
-        die('Unable to connect to database [' . $db->connect_error . ']');
-    }
- ?>
